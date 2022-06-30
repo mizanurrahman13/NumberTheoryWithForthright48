eb4467efcf8418fb160ef0a7576720b213a2eb13
mizanurrahman13 <mizan.441980@gmail.com>
2022-06-30 17:21:08 +0600

    Primality Test Method O(N) and O(√N) Solution

---
+++ src/Console/NumberTheory/PrimalityTestNaiveMethod/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace PrimalityTestNaiveMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //while (true)
+            //{
+            //    var number = int.Parse(Console.ReadLine());
+            //    Console.WriteLine(isPrime(number));
+            //}
+
+            while (true)
+            {
+                var number = int.Parse(Console.ReadLine());
+                Console.WriteLine(isPrimeSqrt(number));
+            }
+        }
+
+        static bool isPrime(int n)
+        {
+            if (n <= 1) return false;
+            for (int i = 2; i < n; i++)
+            {
+                if (n % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        static bool isPrimeSqrt(int n)
+        {
+            if (n <= 1) return false;
+            int sqrtn = (int)Math.Sqrt(n);
+            for (int i = 2; i <= sqrtn; i++)
+            {
+                if (n % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
